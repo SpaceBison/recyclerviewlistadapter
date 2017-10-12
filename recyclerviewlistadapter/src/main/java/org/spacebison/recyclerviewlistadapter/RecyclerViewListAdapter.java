a1739387459c8ae977293038fa12b985f9bf9182
SpaceBison <SpaceBison>
2017-10-12 12:33:54 +0200

    fix hasPrevious and previous

--- recyclerviewlistadapter/src/main/java/org/spacebison/recyclerviewlistadapter/RecyclerViewListAdapter.java
+++ recyclerviewlistadapter/src/main/java/org/spacebison/recyclerviewlistadapter/RecyclerViewListAdapter.java
@@ -387,12 +387,12 @@ public abstract class RecyclerViewListAdapter<T, V extends BindableViewHolder<T>
 
         @Override
         public boolean hasPrevious() {
-            mCurrentIndex = previousIndex();
             return mDelegate.hasPrevious();
         }
 
         @Override
         public T previous() {
+            mCurrentIndex = previousIndex();
             return mDelegate.previous();
         }
 
